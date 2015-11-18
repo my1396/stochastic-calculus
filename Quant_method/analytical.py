0ecdf91328e091a0200ba0a7e7c61acf1aa2dc13
my1396 <my1396@nyu.edu>
2015-11-18 11:40:22 -0500

    second analytical

--- Quant_method/analytical.py
+++ Quant_method/analytical.py
@@ -11,7 +11,10 @@ r=0.01
 sigma=0.2
 T=1
 K=100
+Q=10
 d_=1.0/(sigma*math.sqrt(T))*(math.log(float(s0)/K)+(r-0.5*sigma*sigma)*T)
 d2=1.0/(sigma*math.sqrt(T))*(math.log(float(s0)/K)+(r+0.5*sigma*sigma)*T)
 v=s0*norm.cdf(d2)-math.exp(-r*T)*K*norm.cdf(d_)
+v_binary=math.exp(-r*T)*Q*norm.cdf(d_)
 print "analytical result for Euro Call: ", v
+print "analytical result for Binary Call: ",v_binary
